bde601231e8b8e3955adf8345d141e186b5a18d3
marsyang <bcdex34567890>
2018-11-13 19:02:40 +0800

    update

--- src/main/java/com/gbarbosa/sbs/config/SBSStrutsServlet.java
+++ src/main/java/com/gbarbosa/sbs/config/SBSStrutsServlet.java
@@ -1,10 +1,10 @@
 package com.gbarbosa.sbs.config;
 
+import org.apache.struts2.dispatcher.servlet.StrutsServlet;
+
 import javax.servlet.annotation.WebServlet;
 
-import org.apache.struts2.dispatcher.ng.servlet.StrutsServlet;
-
-@WebServlet(urlPatterns = { "*.do" })
+@WebServlet(urlPatterns = {"*.do"})
 public class SBSStrutsServlet extends StrutsServlet {
 
     private static final long serialVersionUID = 4919365268043339311L;
